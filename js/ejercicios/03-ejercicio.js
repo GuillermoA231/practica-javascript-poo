0913fb551c896116ddb1683678fbc8ef2dcef1e6
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-25 00:36:35 -0300

    Fix: Crea clase del rectangulo

--- js/ejercicios/03-ejercicio.js
+++ js/ejercicios/03-ejercicio.js
@@ -1,31 +1,46 @@
 /*3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área*/
 
-const rectangulo = {
-    alto: 0,
-    ancho: 0,
-    dibujar: function(){
-        let pedirAlto, pedirAncho;
-        do {
-            pedirAlto = prompt("Escriba el alto del rectangulo.");
-            pedirAncho = prompt("Escriba el ancho del rectangulo.");
-        } while (isNaN(pedirAlto) || isNaN(pedirAncho) || pedirAlto < 0 || pedirAncho < 0);
-        this.alto = pedirAlto;
-        this.ancho = pedirAncho;
-    },
-    mostrar:function(){
-        let mostrarPropiedades = `Alto: ${this.alto} - Ancho: ${this.ancho}`;
-        return mostrarPropiedades;
-    },
-    perimetro:function(){
-        let perimetro = this.alto*2 + this.ancho*2;
-        return perimetro;
-    },
-    area:function(){
-        let area = this.alto * this.ancho;
-        return area;
-    }
+let altoUsuario, anchoUsuario;
+class Rectangulo {
+  constructor(alto, ancho) {
+    this._alto = alto;
+    this._ancho = ancho;
+  }
+  set cambiarAlto(valor) {
+    this._alto = valor;
+  }
+  set cambiarAncho(valor) {
+    this._ancho = valor;
+  }
+  get mostrarPropiedades() {
+    let mostrarTexto = `Alto: ${this._alto} - Ancho: ${this._ancho}`;
+    return mostrarTexto;
+  }
+  get perimetro() {
+    return this._alto * 2 + this._ancho * 2;
+  }
+  get area() {
+    return this._alto * this._ancho;
+  }
 }
-rectangulo.dibujar();
-document.write(`<p>${rectangulo.mostrar()}</p>`);
-document.write(`<p>Perimetro: ${rectangulo.perimetro()}</p>`);
-document.write(`<p>Area: ${rectangulo.area()}</p>`);
+
+dibujarRectangulo = (objeto, alto, ancho) => {
+    objeto.cambiarAlto = alto;
+    objeto.cambiarAncho = ancho;
+};
+
+let rectangulo = new Rectangulo();
+
+do {
+  altoUsuario = Number(prompt("Ingrese el alto."));
+  anchoUsuario = Number(prompt("Ingrese el ancho."));
+} while (
+  isNaN(altoUsuario) ||
+  isNaN(anchoUsuario) ||
+  altoUsuario < 0 ||
+  anchoUsuario < 0
+);
+
+dibujarRectangulo(rectangulo, altoUsuario, anchoUsuario);
+
+document.write(`<h1>El rectangulo tiene como propiedades: ${rectangulo.mostrarPropiedades}</h1><h2>El Perimetro del rectangulo es ${rectangulo.perimetro}</h2><h2>El Area del rectangulo es ${rectangulo.area}</h2>`);
